d8a89580c31eeba3bd7f6170b16053b457593194
Dwelch23 <dakotawelch6@gmail.com>
2022-12-21 20:13:22 -0500

    Create myfirst.py
    Learned about types

---
+++ myfirst.py
@@ -0,0 +1,21 @@
+# Intro to types
+
+x = 1 + 1
+print(x)
+
+y = "how"
+print(y)
+
+z = 'a'
+print(z)
+
+str1 = "what"
+str2 = "the"
+space = " "
+print(str1+space+str2)
+print(str1+" "+str2)
+t = True
+f = False
+test = True
+print(not t or f)
+Intro to types
