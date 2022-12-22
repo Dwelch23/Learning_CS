b5f38885a265f7d83d4f65cc79c175158f6673ca
Dwelch23 <dakotawelch6@gmail.com>
2022-12-22 12:48:00 -0500

    learned loops basics
    learned while loops and for loops
    also learned if/else statements

---
+++ loops.py
@@ -0,0 +1,28 @@
+# if statement 
+t = False
+num = 12
+
+if t:
+    print("it printed")
+
+if num < 5:
+    print("lil num")
+elif num > 10:
+    print("big num")
+else:
+    print("medium num")
+
+
+# while loop
+while num < 20:
+    print(num)
+    num += 1
+
+arr = [2, 4, 6, 8, 10, 12, 14]
+print(arr)
+arr.append(1)
+print(arr)
+
+for i in arr:
+    print(i)
+    
--- myfirst.py
+++ myfirst.py
@@ -1,11 +1,14 @@
 # Intro to types
 
+# int
 x = 1 + 1
 print(x)
 
+# string
 y = "how"
 print(y)
 
+# char
 z = 'a'
 print(z)
 
@@ -14,8 +17,9 @@ str2 = "the"
 space = " "
 print(str1+space+str2)
 print(str1+" "+str2)
+
+# booleans
 t = True
 f = False
 test = True
 print(not t or f)
-Intro to types
