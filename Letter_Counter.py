f93f898e33f5595bc6caf04f940a2a0c356cf7cb
Dwelch23 <dakotawelch6@gmail.com>
2022-12-24 14:03:40 -0500

    Made a code to count the letters in a word.

---
+++ Letter_Counter.py
@@ -0,0 +1,8 @@
+user_word = input("type word...")
+
+def count_letters(user_word):
+    num_letters = len(user_word)
+    return num_letters
+
+
+print(count_letters(user_word))
--- palindrome.py
+++ palindrome.py
@@ -11,12 +11,17 @@ def get_backwards_word(word):
         index -= 1
     return output
 
+
+# checks to see if it is a palindrome
+# using backwards word helper function 
 def is_palindrome(word):
     word = word.lower()
     backwards_word = get_backwards_word(word)
     return word == backwards_word
 
 
+# function to check if it is a palendrome
+# using recursion to compare first and last letters
 def is_pal(word):
     word = word.lower()
     if len(word) == 1 or len(word) == 0:
